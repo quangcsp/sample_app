eb165114b51389d797f1989acca3fb01e25c0b62
QUANG <nguyenvanquangcsp@gmai.com>
2016-12-27 17:54:35 +0700

    Homework6

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017131831) do
+ActiveRecord::Schema.define(version: 20161010172113) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -18,9 +19,6 @@ ActiveRecord::Schema.define(version: 20161017131831) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
-    get static_pages_home_url
+    get :home
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get :help
     assert_response :success
   end
 
